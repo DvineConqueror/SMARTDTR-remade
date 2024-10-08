4b5f5b8bb596f291d7ec4b6c48c79018bced3b30
JRPMCendana <151843899+JRPMCendana@users.noreply.github.com>
2024-10-08 17:12:41 +0800

    AHHHHHHHHHHHHHHHHHHHHHHHH

--- app/src/main/java/com/example/smartdtr_remade/teacher__account_details.kt
+++ app/src/main/java/com/example/smartdtr_remade/teacher__account_details.kt
@@ -34,7 +34,7 @@ class teacher__account_details : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        return inflater.inflate(R.layout.fragment_teacher_account_details, container, false)
     }
 
     companion object {
