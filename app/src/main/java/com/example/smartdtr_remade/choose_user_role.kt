ee548faefb56fefe5eefc14a6444b3bd40204b91
JRPMCendana <151843899+JRPMCendana@users.noreply.github.com>
2024-10-08 18:05:06 +0800

    AHHHHHHHHHHHHHHHHHHHHHHHH part 3

--- app/src/main/java/com/example/smartdtr_remade/choose_user_role.kt
+++ app/src/main/java/com/example/smartdtr_remade/choose_user_role.kt
@@ -1,16 +1,44 @@
 package com.example.smartdtr_remade
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.smartdtr_remade.activityStudents.student_login
+import com.example.smartdtr_remade.activityTeachers.teacher_login
 
 class choose_user_role : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_choose_user_role)
+
+        // Using findViewById to initialize buttons
+        val btnStudent: Button = findViewById(R.id.btnStudent)
+        val btnTeacher: Button = findViewById(R.id.btnTeacher)
+
+        btnStudent.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@choose_user_role,
+                    student_login::class.java
+                )
+            )
+        }
+
+        // Redirects for Teacher button
+        btnTeacher.setOnClickListener{
+            startActivity(
+                Intent(
+                    this@choose_user_role,
+                    teacher_login::class.java
+                )
+            )
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/smartdtr_remade/splash_screen.kt
+++ app/src/main/java/com/example/smartdtr_remade/splash_screen.kt
@@ -1,5 +1,6 @@
 package com.example.smartdtr_remade
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,14 @@ class splash_screen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
+
+        android.os.Handler().postDelayed({
+            val intent = Intent(this@splash_screen, choose_user_role::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }, 2000)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
